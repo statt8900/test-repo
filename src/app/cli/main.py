a7e01aa269fc80bb5864ecae29d1b14c72964ffc
Michael Statt <michael.statt@modelyst.io>
2022-07-19 19:59:13 -0400

    initial dependencies

---
+++ src/app/cli/main.py
@@ -0,0 +1,22 @@
+import typer
+from app import __version__
+from rich.console import Console
+
+console = Console()
+app = typer.Typer(name="app", no_args_is_help=True)
+
+
+@app.command("hello")
+def hello_world():
+    """Print the app version."""
+    console.print("hello world")
+
+
+@app.command()
+def version():
+    """Print the app version."""
+    console.print(__version__)
+
+
+if __name__ == "__main__":
+    app()
