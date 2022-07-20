8feaa80e330b6027c5e1c67a49a8b5f34490d99e
Michael Statt <michael.statt@modelyst.io>
2022-07-19 20:41:44 -0400

    add pre-commit

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,3 @@
 """Test App"""
-import numpy as np
 
-__version__ = "0.1.0"
-y = np.max([1])
-
-
-x = "asdflaksjhdflkajhsdlfkjhaslkdjhflakjshdflkjahsdlkjfhalskjdflkajhsdlfhjkasdjkfhalskjdflkjahsdlkjfhasldjkf"
+__version__ = "3.0.2"
