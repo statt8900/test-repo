f113e82e45b095f04551194a4f78713c95bc3eb2
Michael Statt <michael.statt@modelyst.io>
2022-09-28 10:45:17 -0400

    feat: new code

--- src/app/cli/main.py
+++ src/app/cli/main.py
@@ -21,6 +21,12 @@ console = Console()
 app = typer.Typer(name="app", no_args_is_help=True)
 
 
+def unused_function(x: int):
+    y = "hello"
+    z = x + y
+    return z
+
+
 @app.command("hello")
 def hello_world():
     """Print the app version."""
